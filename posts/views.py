64f43372b56d67681e313bc59f3e73d3c9663034
Agiruskiy <hifi91@gmail.com>
2021-06-04 18:27:47 +0300

    0.0.2

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from .models import Post
 # Create your views here.
 
 def first(request):
-    post = Post.objects.all()
-    return render(request, './index.html', {
+    post = Post.objects.order_by("-pub_date")[:11]
+    return render(request, 'index.html', {
         'post' : post
     })
